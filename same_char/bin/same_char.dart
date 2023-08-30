270b0085935c2d3cc809b423e657c2e5d628f17c
Mayasa164 <139007766+Mayasa164@users.noreply.github.com>
2023-08-30 10:32:55 +0400

    same_char

---
+++ same_char/bin/same_char.dart
@@ -0,0 +1,18 @@
+void main(){
+String str1 = "maayasa";
+String str2 = "mayas";
+
+var compare = myList[0];
+
+
+ for (var i = 0; i <= str1.length; i++) {
+   for (var j = 0; j<=str2.length; j++){
+ 
+    if (str1[i]== str2[j] ) {
+        print('The two strings are equal.');
+    }
+    else {       print('The two strings are not equal.');}
+}
+ }
+}
+
---
+++ same_char/test/same_char_test.dart
@@ -0,0 +1,8 @@
+import 'package:same_char/same_char.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
